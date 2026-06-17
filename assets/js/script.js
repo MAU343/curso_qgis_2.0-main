b4274f35ad763976432a471786197b133961d603
Sergio Mauricio Rivera Lara <mauri090604@hotmail.com>
2026-06-17 17:19:46 -0600

    asitencia

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // script.js
 
-// const API_BASE_URL = 'https://sigetux.tuxtla.gob.mx/api';
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://sigetux.tuxtla.gob.mx/api';
+// const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
 
 document.addEventListener('DOMContentLoaded', () => {
